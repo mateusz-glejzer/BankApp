df175fe3e17a66fbcaac23fb5f087624fea4393a
Mateusz <papierekm@gmail.com>
2022-04-30 20:03:00 +0200

    git init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankApp;
+
+public class Program
+{
+    public static void Main()
+    {
+        CSVUsersAndAccountsReader reader = new CSVUsersAndAccountsReader();
+        Bank.Bank mBank = new Bank.Bank(reader);
+        mBank.ReturnAllAccounts();
+        Console.WriteLine("\n\n");
+        mBank.ReturnAccountsOfAnUser(1);
+    }
+}
+
